401321088b83991b95fbf93f83a3b9ffdab96ed0
Huang YenHan <bw@huangde-macbook-pro.local>
2018-11-26 10:49:50 +0800

    Convert to MVC design pattern.

---
+++ Todoey/Data Model/Item.swift
@@ -0,0 +1,15 @@
+//
+//  Item.swift
+//  Todoey
+//
+//  Created by Huang YenHan on 2018/11/25.
+//  Copyright © 2018 HuangYenHan. All rights reserved.
+//
+
+import Foundation
+
+class Item {
+    var title: String = ""
+    var done: Bool = false
+}
+
